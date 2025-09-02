0902db052a1cbbdf17de2f3cedf8ff446d136745
Yechiel Babani <yechielb2000@gmail.com>
2025-09-02 19:20:38 +0300

    remove assigngment producer and assign via redis client

--- services/dispatcher/main.py
+++ services/dispatcher/main.py
@@ -3,21 +3,16 @@ import datetime
 from config.config import config
 from services.dispatcher.matching_strategies.strategy_factory import get_strategy
 from shared.geo import eta_minutes
-from shared.kafka import KafkaConsumer, KafkaProducer
+from shared.kafka import KafkaConsumer
+from shared.logger import logger
 from shared.models import Assignment, Ride, Driver
 from shared.redis_sdk import redis_client
-
-# TODO: not here! load drivers on lifecycle start
 
 ride_consumer = KafkaConsumer[Ride](
     bootstrap_servers=config.kafka.bootstrap_servers,
     group_id=config.dispatcher.group_id,
     topic=config.rides_producer.topic,
     model_cls=Ride,
-)
-assignment_producer = KafkaProducer[Assignment](
-    bootstrap_servers=config.kafka.bootstrap_servers,
-    topic=config.dispatcher.producer.topic,
 )
 
 strategy = get_strategy(config.dispatcher.strategy)
@@ -33,25 +28,24 @@ def main():
         available_drivers = redis_client.driver.list_available()
         available_drivers = available_drivers.filter_by_vehicle_type(vehicle_type=ride.vehicle_type)
         if not available_drivers:
-            # TODO: replace with log
-            print(f"No available driver for ride {ride.id}")
+            logger.info("No available drivers")
+            redis_client.metrics.add_unassigned(ride_id=ride.id)
             continue
 
         selected_driver = strategy(redis_client.driver).match(ride, available_drivers)
         if not selected_driver:
-            # TODO: replace with log
-            print(f"No suitable driver found for ride {ride.id}")
+            logger.info("No suitable driver found for ride", ride_id=ride.id)
+            redis_client.metrics.add_unassigned(ride_id=ride.id)
             continue
 
         set_drive_busy(ride, selected_driver)
-
         assignment = Assignment(
             ride_id=ride.id,
             ride_request_time=ride.timestamp,
             driver_id=selected_driver.id,
             timestamp=redis_client.clock.get()
         )
-        assignment_producer.send(assignment)
+        redis_client.metrics.add_assignment(assignment)
 
 
 def set_drivers_free():
