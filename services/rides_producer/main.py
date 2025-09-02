8d6829de5c4896c6085f3f048f2f6031b65579e1
Yechiel Babani <yechielb2000@gmail.com>
2025-09-03 01:31:55 +0300

    make sure to have current ride timestamp when producing it

--- services/rides_producer/main.py
+++ services/rides_producer/main.py
@@ -4,12 +4,13 @@ from config.config import config
 from services.rides_producer.loader import load_rides
 from shared.kafka.producer import KafkaProducer
 from shared.models import Ride
-
+import datetime
 
 def main():
     ride_producer = KafkaProducer[Ride](config.kafka.bootstrap_servers, config.rides_producer.topic)
     for ride in load_rides(config.rides_producer.json_file):
         sleep(config.rides_producer.sim_speed)
+        ride.timestamp = datetime.datetime.now()
         ride_producer.send(ride)
 
 
