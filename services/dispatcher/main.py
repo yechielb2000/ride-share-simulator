02deafe7745ad792bb5a7cb88ca42a6308b6d57a
Yechiel Babani <yechielb2000@gmail.com>
2025-09-03 12:01:00 +0300

    add comment

--- services/dispatcher/main.py
+++ services/dispatcher/main.py
@@ -52,8 +52,9 @@ def set_drivers_free():
     """
     set free drivers that are no longer busy
     """
-    for driver in redis_client.driver.list_all():
-        if driver.busy and driver.eta and driver.eta <= redis_client.clock.get():
+    for driver in redis_client.driver.list_unavailable():
+        # not sure if I need to check for driver.eta but for the sake of it...
+        if driver.eta and driver.eta <= redis_client.clock.get():
             redis_client.driver.mark_free(driver.id)
 
 
