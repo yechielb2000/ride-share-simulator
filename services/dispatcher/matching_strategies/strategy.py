24524f62f38aed171a44b5ebbba8195c672abf9a
Yechiel Babani <yechielb2000@gmail.com>
2025-09-04 13:23:13 +0300

    remove drivers sdk from startegy interface

--- services/dispatcher/matching_strategies/strategy.py
+++ services/dispatcher/matching_strategies/strategy.py
@@ -2,16 +2,12 @@ from abc import ABC, abstractmethod
 
 from shared.models import Ride, Driver
 from shared.models.driver import Drivers
-from shared.redis_sdk import DriverRedisSDK
 
 
 class MatchingStrategy(ABC):
     """
     interface for all matching strategies.
     """
-
-    def __init__(self, driver_sdk: DriverRedisSDK):
-        self.driver_sdk = driver_sdk
 
     @abstractmethod
     def match(self, ride: Ride, available_drivers: Drivers) -> Driver | None:
